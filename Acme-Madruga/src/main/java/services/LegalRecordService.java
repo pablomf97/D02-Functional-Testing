98cb6ea1971f4c948d870296255f30e7b0faf933
jesusrico12 <jesus9610@gmail.com>
2019-03-19 19:09:08 +0100

    level C completed

--- Acme-Madruga/src/main/java/services/LegalRecordService.java
+++ Acme-Madruga/src/main/java/services/LegalRecordService.java
@@ -61,6 +61,7 @@ public class LegalRecordService {
 
 	public LegalRecord save(LegalRecord legalRecord){
 		LegalRecord res;
+		LegalRecord copy;
 		Brotherhood principal;
 		Collection<LegalRecord> legals;
 		History historyBro;
@@ -77,12 +78,25 @@ public class LegalRecordService {
 		historyBro = principal.getHistory();
 		Assert.notNull(historyBro);
 		legals=historyBro.getLegalRecords();
+		if(legalRecord.getId()!=0){
+			copy=this.findOne(legalRecord.getId());
+			copy.setTitle(legalRecord.getTitle());
+			copy.setDescription(legalRecord.getDescription());
+			copy.setLaws(legalRecord.getLaws());
+			copy.setName(legalRecord.getName());
+			copy.setVAT(legalRecord.getVAT());
+			copy.setVersion(legalRecord.getVersion());
+			copy.setId(legalRecord.getId());
 
-		res=this.legalRecordRepository.save(legalRecord);
-		if(legalRecord.getId() == 0){
-			legals.add(legalRecord);
-			historyBro.setLegalRecords(legals);
+			res=this.legalRecordRepository.save(copy);
 		}
+		else{
+
+			res=this.legalRecordRepository.save(legalRecord);
+			legals.add(res);
+
+		}		
+
 		Assert.notNull(res);
 		return res;
 	}
@@ -100,15 +114,15 @@ public class LegalRecordService {
 		//Assert.isTrue(legalRecord.getId()==0 , "wrong.id");
 		historyBro = principal.getHistory();
 		legals=historyBro.getLegalRecords();
-		this.legalRecordRepository.delete(legalRecord);
-		legals.remove(legalRecord);
+		legals.remove(this.findOne(legalRecord.getId()));
+		this.legalRecordRepository.delete(this.findOne(legalRecord.getId()));
 		Assert.notNull(historyBro);
-	
+
 	}
 	public Collection<String> getSplitLaws(final String laws) {
 		final Collection<String> res = new ArrayList<>();
 		final String[] slice = laws.split(",");
-		
+
 		for (final String p : slice){
 			if (p.trim() != ""){
 				res.add(p);
