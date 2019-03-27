82d21b3d5e02dbeda8637731e9d6fc56f1c58518
jesusrico12 <jesus9610@gmail.com>
2019-03-28 00:00:29 +0100

    test level B and fix bug dashboard level B

--- Acme-Madruga/src/test/java/sample/ChapterServiceTest.java
+++ Acme-Madruga/src/test/java/sample/ChapterServiceTest.java
@@ -8,7 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+import domain.Chapter;
+import domain.Zone;
+
+import repositories.ChapterRepository;
+
 import services.ChapterService;
+import services.ZoneService;
 
 import utilities.AbstractTest;
 
@@ -20,29 +29,55 @@ import utilities.AbstractTest;
 public class ChapterServiceTest extends AbstractTest {
 	
 	//System under test ---------------------------------------
-
-
+		@Autowired 
+		private Validator validator;
+		
+		
 		@Autowired
 		private ChapterService chapterService;
+		
+		@Autowired
+		private ZoneService zoneService;
+		
+		@Autowired
+		private ChapterRepository chapterRepository;
 
 		//Tests ----------------------------------------------------
 
-		//Testing UC 7.1
 		
+		
+		//Test: Caso de uso: 
+		//An actor who is not authenticated must be able to:
+		// Register to the system as a chapter. (7.1)
 		@Test
 		public void driver(){
 			Object testingData[][] = {
-					{null, null},//positive
+					{null,"Manantial 5" ,"carlos12@gmail.com","carl","618922569","https://www.google.com/img=219","Rico","Carlos","Chapter","zone1",null},//Positivo:registro normal
+					{null,null ,"carlos12@gmail.com",null,null,"https://www.google.com/img=219","Rico","Carlos","Chapter","zone2",null},//Positivo:campos no obligatorios nulos
+					{null,"Manantial 5" ,"carlos12@gmail.com","carl","618922569","https://www.google.com/img=219","Rico","Carlos","","zone1",java.lang.NullPointerException.class},//Negativo->RN:Title no puedes ser notBlank
+					{null,"Manantial 5" ,"carlos12@gmail.com","carl","618922569","https://www.google.com/img=219","Rico","Carlos",null,"zone1",java.lang.NullPointerException.class},//Negativo->RN:Title no puedes ser notNull
+					{null,"Manantial 5" ,"carlos12@gmail.com","carl","618922569","url@aa","Rico","Carlos","Chapter","zone1",java.lang.NullPointerException.class},//Negativo->RN:Url no tiene el formato adecuado
+					{null,"Manantial 5" ,null,"carl","618922569","https://www.google.com/img=219",null,null,"Chapter","zone1",java.lang.NullPointerException.class},//Negativo->RN:nombre, correo, apellidos no pueden ser nulos
+					{"vxv","Manantial 5" ,"carlos12@gmail.com","carl","618922569","https://www.google.com/img=219","Rico","Carlos","Chapter","zone1",IllegalArgumentException.class},//Negativo->Intento de registro logueado como un actor no existente en la DB
 					
 			};
 
 			for(int i = 0; i < testingData.length; i++){
 				template((String)testingData[i][0],
-						(Class<?>) testingData[i][1]);
+						(String)testingData[i][1],
+						(String)testingData[i][2],
+						(String)testingData[i][3],
+						(String)testingData[i][4],
+						(String)testingData[i][5],
+						(String)testingData[i][6], 
+						(String)testingData[i][7],
+						(String)testingData[i][8],
+						(int) super.getEntityId((String)testingData[i][9]),
+						(Class<?>) testingData[i][10]);
 			}
 		}
 		
-		protected void template(String username, Class<?> expected){
+		protected void template(String username, String Address, String Email, String MiddleName, String PhoneNumber, String Photo, String Surname, String Name, String Title, int zone, Class<?> expected){
 			Class<?> caught;
 
 			caught = null;
@@ -50,7 +85,9 @@ public class ChapterServiceTest extends AbstractTest {
 
 			try{
 				authenticate(username);
-				
+	
+				Zone z=this.zoneService.findOne(zone);
+				this.registerChapter(Address,Email,MiddleName,PhoneNumber,Photo,Surname,Name,Title,z);
 				unauthenticate();
 			}catch(Throwable oops){
 				caught = oops.getClass();
@@ -59,6 +96,29 @@ public class ChapterServiceTest extends AbstractTest {
 
 			super.checkExceptions(expected, caught);
 		}
+		
+		public void registerChapter(String address, String email, String middleName, String phoneNumber, String photo, String surname, String name, String title, Zone z){
+
+			final Chapter principal =this.chapterService.create();
+			BindingResult binding=null;
+				
+
+				principal.setAddress(address);
+				principal.setEmail(email);
+				principal.setMiddleName(middleName);
+				principal.setPhoneNumber(phoneNumber);
+				principal.setPhoto(photo);
+				principal.setSurname(surname);
+				principal.setName(name);
+				principal.setTitle(title);
+				principal.setZone(z);
+				
+				
+				this.validator.validate(principal, binding);
+				this.chapterService.save(principal);
+				this.chapterRepository.flush();
+		
+		}		
 
 
 }
