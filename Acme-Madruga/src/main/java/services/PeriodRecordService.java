33f0d94f432ce6fd5b911af79b565e0bc43f4c43
pablomf97 <pablomf@hotmail.es>
2019-03-28 16:24:22 +0100

    Fixes in Tests and Services

--- Acme-Madruga/src/main/java/services/PeriodRecordService.java
+++ Acme-Madruga/src/main/java/services/PeriodRecordService.java
@@ -1,4 +1,3 @@
-
 package services;
 
 import java.util.ArrayList;
@@ -20,11 +19,10 @@ import domain.PeriodRecord;
 public class PeriodRecordService {
 
 	@Autowired
-	private PeriodRecordRepository	periodRecordRepository;
+	private PeriodRecordRepository periodRecordRepository;
 
 	@Autowired
-	private ActorService			actorService;
-
+	private ActorService actorService;
 
 	public PeriodRecord findOne(final int id) {
 		PeriodRecord res;
@@ -41,7 +39,9 @@ public class PeriodRecordService {
 
 		Brotherhood principal;
 		principal = (Brotherhood) this.actorService.findByPrincipal();
-		Assert.isTrue(this.actorService.checkAuthority(principal, "BROTHERHOOD"), "not.allowed");
+		Assert.isTrue(
+				this.actorService.checkAuthority(principal, "BROTHERHOOD"),
+				"not.allowed");
 		final PeriodRecord res = new PeriodRecord();
 		return res;
 
@@ -51,9 +51,11 @@ public class PeriodRecordService {
 		PeriodRecord res;
 		Brotherhood principal;
 		History historyBro;
-		//Collection<PeriodRecord> periodRecords;
+		// Collection<PeriodRecord> periodRecords;
 		principal = (Brotherhood) this.actorService.findByPrincipal();
-		Assert.isTrue(this.actorService.checkAuthority(principal, "BROTHERHOOD"), "not.allowed");
+		Assert.isTrue(
+				this.actorService.checkAuthority(principal, "BROTHERHOOD"),
+				"not.allowed");
 		Assert.notNull(periodRecord.getTitle(), "not.null");
 		Assert.notNull(periodRecord.getDescription(), "not.null");
 		Assert.notNull(periodRecord.getStartYear(), "not.null");
@@ -62,28 +64,36 @@ public class PeriodRecordService {
 		this.checkYear(periodRecord);
 		historyBro = principal.getHistory();
 		Assert.notNull(historyBro);
-		//periodRecords=historyBro.getPeriodRecords();
-		//periodRecord.setTitle(periodRecord.getTitle());
-		//periodRecord.setDescription(periodRecord.getDescription());
-		//periodRecord.setStartYear(periodRecord.getStartYear());
-		//periodRecord.setEndYear(periodRecord.getEndYear());
-		//periodRecord.setPhotos(periodRecord.getPhotos());
+		// periodRecords=historyBro.getPeriodRecords();
+		// periodRecord.setTitle(periodRecord.getTitle());
+		// periodRecord.setDescription(periodRecord.getDescription());
+		// periodRecord.setStartYear(periodRecord.getStartYear());
+		// periodRecord.setEndYear(periodRecord.getEndYear());
+		// periodRecord.setPhotos(periodRecord.getPhotos());
 		res = this.periodRecordRepository.save(periodRecord);
 		if (periodRecord.getId() == 0)
 			historyBro.getPeriodRecords().add(periodRecord);
 		Assert.notNull(res);
 		return res;
 	}
+
 	public void checkYear(final PeriodRecord periodRecord) {
-		Assert.isTrue(periodRecord.getStartYear() <= (periodRecord.getEndYear()), "not.date");
-		Assert.isTrue(periodRecord.getStartYear() > 1500 && (periodRecord.getEndYear() < 2100), "not.date");
+		Assert.isTrue(
+				periodRecord.getStartYear() <= (periodRecord.getEndYear()),
+				"not.date");
+		Assert.isTrue(
+				periodRecord.getStartYear() > 1500
+						&& (periodRecord.getEndYear() < 2100), "not.date");
 	}
+
 	public void delete(final PeriodRecord periodRecord) {
 		Brotherhood principal;
 		History historyBro;
 		Collection<PeriodRecord> periods;
 		principal = (Brotherhood) this.actorService.findByPrincipal();
-		Assert.isTrue(this.actorService.checkAuthority(principal, "BROTHERHOOD"), "not.allowed");
+		Assert.isTrue(
+				this.actorService.checkAuthority(principal, "BROTHERHOOD"),
+				"not.allowed");
 
 		Assert.notNull(periodRecord);
 		historyBro = principal.getHistory();
